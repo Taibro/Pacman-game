d1b0d0ae8c7646ff2de23b5767c5f2e0054fef3a
Tai <ntai8448@gmail.com>
2025-10-13 21:15:43 +0700

    Add src code

--- pacman.py
+++ pacman.py
@@ -28,9 +28,22 @@ direction_command = 0
 player_speed = 2
 score = 0
 
-def check_collisions():
+def draw_misc():
+    score_text = font.render(f'Score: {score}', True,'white')
+    screen.blit(score_text, (10, 920))
+
+def check_collisions(scor):
+    num1 = (HEIGHT - 50) // 32
+    num2 = WIDTH//30
     if 0 < player_x < 870:
-        if level[]
+        if level[center_y // num1][center_x // num2] == 1:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 10
+        if level[center_y // num1][center_x // num2] == 2:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 50
+            
+    return scor
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -148,9 +161,10 @@ while run:
     screen.fill("black")
     draw_board()
     draw_player()
+    draw_misc()
     center_x = player_x + 23
     center_y = player_y + 24
-    pygame.draw.circle(screen, 'white', (center_x, center_y), 2)
+    #pygame.draw.circle(screen, 'white', (center_x, center_y), 2)
     turns_allowed = check_position(center_x, center_y)
     player_x, player_y = move_player(player_x, player_y)
     score = check_collisions(score)
